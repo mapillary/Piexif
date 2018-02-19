b03d1470aa0eebd1d87915505a191cbedf393fd3
Hinko Kocevar <hinkocevar@gmail.com>
2018-02-19 14:21:14 +0100

    Exif data is always in APP1 segment - can be any index.
    In order to handle JFIF and Exif segments, we can not respect specs
    saying that Exif segment must follow SOI. JFIF also wants to follow SOI.
    Solution is to be more roboust and bend the specs a bit.

--- piexif/_common.py
+++ piexif/_common.py
@@ -74,28 +74,20 @@ def get_exif_seg(segments):
 
 
 def merge_segments(segments, exif=b""):
-    """Merges Exif with APP0 and APP1 manipulations.
+    """Merges Exif with APP1 manipulations.
     """
-    if segments[1][0:2] == b"\xff\xe0" and \
-       segments[2][0:2] == b"\xff\xe1" and \
-       segments[2][4:10] == b"Exif\x00\x00":
-        if exif:
-            segments[2] = exif
-            segments.pop(1)
-        elif exif is None:
-            segments.pop(2)
-        else:
-            segments.pop(1)
-    elif segments[1][0:2] == b"\xff\xe0":
-        if exif:
-            segments[1] = exif
-    elif segments[1][0:2] == b"\xff\xe1" and \
-         segments[1][4:10] == b"Exif\x00\x00":
-        if exif:
-            segments[1] = exif
-        elif exif is None:
-            segments.pop(1)
-    else:
-        if exif:
-            segments.insert(1, exif)
+    found = False
+    for i in range(0, len(segments)):
+        if segments[i][0:2] == b"\xff\xe1" and \
+           segments[i][4:10] == b"Exif\x00\x00":
+            if not found and exif is not None:
+                # replace first occurence of APP1:Exif segment
+                # print("REPLACING Exif seg %d.." % i)
+                segments[i] = exif
+                found = True
+            else:
+                # remove additional occurences of APP1:Exif segment
+                # print("REMOVE Exif seg %d.." % i)
+                segments.pop(i)
     return b"".join(segments)
+        
